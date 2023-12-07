4a5b5469dbf7cfe13d5be9369fcd606659397b5d
matteocicigoi <matteo.cicigoi1@hotmail.it>
2023-12-07 18:15:35 +0100

    feat: terminato

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,41 @@
 import HeaderComponent from './components/header/HeaderComponent.vue';
 // Footer
 import FooterComponent from './components/footer/FooterComponent.vue';
+// Main
+import ParteAzzurra from './components/main/ParteAzzurra.vue';
 
 export default{
   components : {
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ParteAzzurra
   }
 }
 </script>
 
 <template>
   <HeaderComponent />
+  <div class="black"><h2>Content goes here</h2></div>
+  <main>
+    <ParteAzzurra />
+  </main>
   <FooterComponent />
 </template>
 
 <style scoped lang="scss">
 @use './style/style.scss' as *;
+  main {
+    background-color: #0282F9;
+    padding: 50px 0;
+
+    > div {
+        width: 1280px;
+        margin: auto;
+    }
+  }
+  .black{
+      background-color: #1C1C1C;
+      color: white;
+      padding: 55px;
+  }
 </style>
---
+++ src/components/main/ParteAzzurra.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+    data() {
+        return {
+            list: [
+                {
+                    name : 'digital comics',
+                    link : '#',
+                    img: 'buy-comics-digital-comics.png'
+
+                },
+                {
+                    name : 'dc merchandise',
+                    link : '#',
+                    img: 'buy-comics-merchandise.png'
+
+                },
+                {
+                    name : 'subscription',
+                    link : '#',
+                    img: 'buy-comics-subscriptions.png'
+
+                },
+                {
+                    name : 'comic shop locator',
+                    link : '#',
+                    img: 'buy-comics-shop-locator.png'
+
+                },
+                {
+                    name : 'dc power visa',
+                    link : '#',
+                    img: 'buy-dc-power-visa.svg'
+
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <ul class="list-element">
+        <li v-for="element in list">
+            <a :href="element.link">
+                <div class="img">
+                    <img :src="`/public/img/${element.img}`" :alt="element.name">
+                </div>
+                <span>{{ element.name }}</span>
+            </a>
+        </li>
+    </ul>
+    </div>
+</template>
+<style lang="scss">
+    .list-element{
+        list-style-type: none;
+        display: flex;
+        justify-content: space-around;
+        
+        li {
+            a {
+                display: flex;
+                align-items: center;
+                text-decoration: none;
+                
+                .img {
+                    height: 55px;
+                    width: 55px;
+                    display: block;
+                    margin-right: 15px;
+
+                    img {
+                        object-fit: contain;
+                        width: 100%;
+                        height: 100%;
+                    }
+                }
+                span {
+                    text-transform: uppercase;
+                    color: white;
+                }
+            }
+        }
+    }
+</style>
